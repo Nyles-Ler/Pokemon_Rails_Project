cf3e8b42ac8ea35ad709f86efc6a609d6bf41f4f
Jordan Lerat <jlerat@rrc.ca>
2026-07-02 13:26:28 -0500

    Added Active Record model associations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,13 @@
 class Pokemon < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  has_many :pokemon_types
+  has_many :types, through: :pokemon_types
+
+  has_many :pokemon_abilities
+  has_many :abilities, through: :pokemon_abilities
+
+  has_many :pokemon_moves
+  has_many :moves, through: :pokemon_moves
+
 end
